c29d359624ce0fd7ce3c6e262f65e134dcae143e
Nilanchal <nilanchalpanda2003@gmail.com>
2023-09-10 11:01:55 +0530

    Added header, footer, menu, and pizza components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,46 @@
-
-
 function App() {
   return (
     <>
-      <h1>HELLO WORLD, my name is NILANCHALA PANDA. How are you !?</h1>
+      {/* <h1>HELLO WORLD, my name is NILANCHALA PANDA. How are you !?</h1> */}
+      <Header />
+      <Pizza />
+      <Menu />
+      <Footer />
     </>
   );
+}
+
+function Header(){
+  return(
+    <>
+      <h1> This is the Header of the file </h1>
+    </>
+  )
+}
+
+function Pizza(){
+  return(
+    <>
+      <img src="/public/pizzas/focacci.jpg" alt="Focaccia" ></img>
+      <h2>This is the pizza component :)</h2>
+    </>
+  )
+}
+
+function Menu() {
+  return(
+    <>
+      <h1> MENU </h1>
+    </>
+  )
+}
+
+function Footer() {
+  return(
+    <>
+      <h1> Footer </h1>
+    </>
+  )
 }
 
 export default App;
